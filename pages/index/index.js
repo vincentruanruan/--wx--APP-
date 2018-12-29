b4577e600af044cf145a40cb59197b696489c14d
vincentruan <ruan81@vip.qq.com>
2018-12-29 17:36:07 +0800

    首页修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,115 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgUrls: [
+      '/imgs/banner1.png',
+      '/imgs/banner2.png',
+      '/imgs/banner3.png'
+    ],
+    circular: true,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 300,
+    indicatorColor: 'rgba(255,255,255,0.5)',
+    indicatorActiveColor: '#ffffff',
+    bannerHeight: [],
+    current: 0,
+
+    server: [{
+      url: '/imgs/server1.png',
+      title: '微信商城',
+      desc: '资深研发团队，提供高品质微信开发服务'
+    }, {
+      url: '/imgs/server2.png',
+      title: '小程序开发',
+      desc: '专业技术团队,专业设计流程,全行业定制开发'
+    }, {
+      url: '/imgs/server3.png',
+      title: 'APP开发',
+      desc: '专业定制IOS/安卓APP软件，专业APP设计服务'
+    }, {
+      url: '/imgs/server4.png',
+      title: '企业官网',
+      desc: '专业定制高端企业网站，专业企业官网设计'
+      }],
+
+    develop: [{
+      url: '/imgs/develop1.png',
+      title: '需求沟通',
+      desc: '客户提出需求，进行功能分析，确定功能需求'
+    }, {
+      url: '/imgs/develop2.png',
+      title: '交互设置',
+      desc: '根据已确定的需求功能确认原型，UI设计稿'
+    }, {
+      url: '/imgs/develop3.png',
+      title: '技术开发',
+      desc: '根据客户需求，完成前后端功能开发'
+    }, {
+      url: '/imgs/develop4.png',
+      title: '发布上架',
+      desc: '与客户确定验收无误后发布上架'
+    }, {
+      url: '/imgs/develop5.png',
+      title: '售后跟进',
+      desc: '专业售后跟进服务，后续持续跟进客户需求'
+      }],
+
+    recommend: [{
+      url: '/imgs/case1.png',
+      title: '需求沟通',
+    }, {
+        url: '/imgs/case1.png',
+      title: '交互设置',
+    }, {
+        url: '/imgs/case1.png',
+      title: '技术开发',
+    }, {
+        url: '/imgs/case1.png',
+      title: '发布上架',
+    }]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  changeIndicatorDots(e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      indicatorDots: !this.data.indicatorDots
     })
-  }
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  imageLoad: function(e) { //获取图片真实宽度  
+    var imgwidth = e.detail.width,
+      imgheight = e.detail.height,
+      //宽高比  
+      ratio = imgwidth / imgheight;
+    // console.log(imgwidth, imgheight)
+    //计算的高度值  
+    var viewHeight = 750 / ratio;
+    var imgheight = viewHeight;
+    var imgheights = this.data.bannerHeight;
+    //把每一张图片的对应的高度记录到数组里  
+    imgheights[e.target.dataset.id] = imgheight;
+    this.setData({
+      bannerHeight: imgheights
+    })
+  },
+  bindchange: function(e) {
+    // console.log(e.detail.current)
+    this.setData({
+      current: e.detail.current
+    })
+  },
 })
